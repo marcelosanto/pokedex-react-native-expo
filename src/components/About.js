d4eae5865e9645d1407a464a917dd559725a85f6
marcelosanto <rocha.mer21@gmail.com>
2021-11-24 18:20:22 -0300

    bug: align elements

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,60 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default () => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={{ flex: 1, marginLeft: 20, width: 300 }}>
+      <View style={{ marginTop: 20, marginBottom: 20 }}>
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Species</Text>
+          <Text style={styles.textInfo}>Seed</Text>
+        </View>
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Height</Text>
+          <Text style={styles.textInfo}> 2'3.6* (0,70cm)</Text>
+        </View>
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Weight</Text>
+          <Text style={styles.textInfo}>15.2lbs (6.9Kg)</Text>
+        </View>
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Abilities</Text>
+          <Text style={styles.textInfo}>Overgrow, Chlorophyl</Text>
+        </View>
+      </View>
+      <View style={{}}>
+        <Text style={styles.textTitle}>Breeding</Text>
+
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Gender</Text>
+          <Text style={styles.textInfo}>Male and Femea</Text>
+        </View>
+
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Egg Groups</Text>
+          <Text style={styles.textInfo}>Monster</Text>
+        </View>
+        <View style={styles.containerDetails}>
+          <Text style={styles.text}>Egg Cycle</Text>
+          <Text style={styles.textInfo}>Grass</Text>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  containerDetails: {
+    flexDirection: 'row',
+  },
+  text: { marginBottom: 20 },
+  textTitle: {
+    marginTop: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  textInfo: {
+    marginLeft: 80,
+  },
+})
